193f97e3401ee938f5b916a2dbb6935e61ba90c8
Galaxyqasar <kunze.georg44@gmail.com>
2018-10-28 20:37:55 +0100

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1478374600,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/backgroundtile.png",
+		"images/player-sheet0.png",
+		"images/solidtile.png",
+		"images/movingsolidtile.png",
+		"images/jumpthrutile.png",
+		"images/sprite-sheet0.png",
+		"images/sprite11-sheet0.png",
+		"images/sprite21-sheet0.png",
+		"images/sprite2-sheet0.png"
+	]
+}
